d40b26fae460ffb60fad18d0e4bb1d5c75b271d9
Dastan <ddbolatkhan@gmail.com>
2025-05-23 22:59:57 +0500

    сделал вьюхи и шаблоны

--- shop/main/views.py
+++ shop/main/views.py
@@ -3,16 +3,24 @@ from .models import Category, Product
 
 def product_list(request, category_slug = None):
     categories = Category.objects.all()
-    products = Products.objects.filter(available = True)
+    products = Product.objects.filter(available = True)
 
     category = None
     if category_slug:
-        category = get_object_or_404(Category, category_slug = category_slug)
+        category = get_object_or_404(Category, slug = category_slug)
         products = products.filter(category = category)
     
     return render(request, 'main/product/list.html', 
                     {'category': category, 
                     'categories': categories, 
                     'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(PRoduct, id = id, slug = slug, available = True)
+    related_products = Product.objects.filter(category = product.category, 
+                                                available = True).exlude(id = product.id)[:4]
+    
+    return render(request, 'main/product/detail.html', {'product': product , 
+                                                        'related_products': related_products})
 
 
